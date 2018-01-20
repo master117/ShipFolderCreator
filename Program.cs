7acd204cdffaafcf7da46241b16ad2b3e6236f71
Johannes Gocke <johannes_gocke@hotmail.de>
2018-01-20 23:36:47 +0100

    Finalized Programm

---
+++ Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShipFolderCreator
+{
+    class Program
+    {
+        //This tool creates folders according to all size 2 permutations of a list, in alphabetic order
+        static void Main(string[] args)
+        {
+            //Create List
+            List<string> candidates = new List<string>()
+            {
+                "Honoka",
+                "Kotori",
+                "Umi",
+                "Hanayo",
+                "Maki",
+                "Rin",
+                "Nico",
+                "Nozomi",
+                "Eli",
+                "Chika",
+                "You",
+                "Riko",
+                "Hanamaru",
+                "Ruby",
+                "Yoshiko",
+                "Mari",
+                "Dia",
+                "Kanan"
+            };
+
+            //Sort Alphabetically
+            candidates.Sort();
+
+            //2 Loops for size 2
+            // Go 1 -> n-1
+            for(int i = 0; i < candidates.Count - 1; i++)
+            {
+                //Go 2 -> n
+                for(int j = i + 1; j < candidates.Count; j++)
+                {
+                    Directory.CreateDirectory(candidates[i] + " x " + candidates[j]);
+                }
+            }
+        }
+    }
+}
